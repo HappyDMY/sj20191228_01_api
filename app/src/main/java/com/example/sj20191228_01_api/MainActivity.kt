c201cb917bd52d63256376a3afb4a8375cd06406
jun <vagrantsj@gmail.com>
2019-12-28 14:41:50 +0900

    로그아웃 버튼 누르면 alert으로 의사확인

--- app/src/main/java/com/example/sj20191228_01_api/MainActivity.kt
+++ app/src/main/java/com/example/sj20191228_01_api/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.sj20191228_01_api
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +16,19 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+        logOutBtn.setOnClickListener {
+            val alert = AlertDialog.Builder(mContext)
+
+            alert.setTitle("LogOut")
+            alert.setMessage("정말 로그아웃 할래요?")
+            alert.setPositiveButton("확인", DialogInterface.OnClickListener { dialog, which ->
+                Toast.makeText(mContext, "로그아웃", Toast.LENGTH_SHORT).show()
+            })
+            alert.setNegativeButton("취소", null) // 취소는 눌러도 아무일도 없도록
+
+            alert.show()
+
+        }
     }
 
     override fun setValues() {
